877f92d72d27f670b71d9deed3635fea9ef318cc
ch3z <73852019+chezdev21@users.noreply.github.com>
2025-04-07 22:37:30 +0100

    Removed redundant Enrolment functionality

--- src/Enrolment.java
+++ src/Enrolment.java
@@ -1,6 +1,5 @@
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Enrolment extends College {
@@ -20,7 +19,6 @@ public class Enrolment extends College {
         this.enrolDate = enrolDate;
     }
 
-
     protected int getStudentID() {
         return studentID;
     }
@@ -38,41 +36,6 @@ public class Enrolment extends College {
     public void setDate(GregorianCalendar enrolDate) {
         this.enrolDate = enrolDate;
     }
-
-    /*
-    // Object referencing via passing Student and Course classes as parameters
-    public void enrolAdd(String studentQuery, String courseQuery){
-        Iterator<Student> studentIt = studentList.iterator();
-        Iterator<Course> courseIt = courseList.iterator();
-        boolean validentry = false;
-        while (studentIt.hasNext() && courseIt.hasNext()){
-            studentIt.next(); courseIt.next();
-            if (studentList.contains(studentQuery) && courseList.contains(courseQuery)
-            ){
-                int foundindex = studentList.get(Integer.parseInt(studentIt.toString())).getStudentID();
-                enrolList.add(Integer.parseInt(studentQuery), Integer.parseInt(courseQuery));
-                validentry = true;
-                System.out.println(studentList.get(foundindex).getName());
-            }
-        }
-    }
-
-    public void withdraw(int studentQuery, int courseQuery){
-        int i = 0;
-        boolean found = false;
-        while (i < enrolList.size() && !found){
-            if (enrolList.contains(studentQuery) && enrolList.contains(courseQuery) ){
-                enrolList.remove(i);
-                found = true;
-                System.out.println("Student with the ID: " + studentQuery + "and Course ID: " + courseQuery + "was found and removed.");
-            }
-            if (!found){
-                System.out.println("Student with the ID: " + studentQuery + "and Course ID: " + courseQuery + "was not found.");
-            }
-        }
-    }
-
-     */
 
     public String displayRecord() {
         return "Student with the ID: " + getStudentID() + " has been enrolled in course: " + "CRS-" + getCourseID() + " since " + getDate();
